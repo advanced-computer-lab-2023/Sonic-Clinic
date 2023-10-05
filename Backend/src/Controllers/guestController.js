c8d9e0cfc38ac0e1a55ef98ab490722636d1663b
SarahTawfik <sarahtawfik0447@gmail.com>
2023-10-05 18:33:35 +0300

    guest controller

---
+++ Backend/src/Controllers/guestController.js
@@ -0,0 +1,22 @@
+const { default: mongoose } = require('mongoose');
+
+
+const PotentialDoctorModel = require('../Models/PotentialDoctor.js');
+const patientModel = require('../Models/Patient.js');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = {};
