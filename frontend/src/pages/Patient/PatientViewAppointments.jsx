e4e29e6021cd811d9c404f6e6863efb837b2c6a6
youssefbassem1 <youssefbassem12@gmail.com>
2023-10-09 02:47:08 +0300

    All view done/missing filters

--- frontend/src/pages/Patient/PatientViewAppointments.jsx
+++ frontend/src/pages/Patient/PatientViewAppointments.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import AppNavbar from "../../components/AppNavigation/AppNavbar";
 import HamburgerMenu from "../../components/Patient/HamburgerMenu";
 import ShowAppointments from "../../components/Patient/ShowAppointments";
@@ -12,17 +12,20 @@ function PatientViewAppointments() {
   useEffect(() => {
     dispatch(deleteSearchData());
   }, []);
+
   return (
     <div>
       <AppNavbar hamburgerMenu={<HamburgerMenu />} />
       <Container fluid className="bg-light pt-3 mt-2">
-        <Container className="bg-white px-5 py-4 d-flex align-items-center justify-content-center">
-          <div className="col-5">
-            <AppointmentFilter />
-          </div>
-          <div className="col-7">
-            <ShowAppointments />
-          </div>
+        <Container className="bg-white px-5 py-4">
+          <Row>
+            <Col xs={12} md={5}>
+              <AppointmentFilter />
+            </Col>
+            <Col xs={12} md={7}>
+              <ShowAppointments />
+            </Col>
+          </Row>
         </Container>
       </Container>
     </div>
