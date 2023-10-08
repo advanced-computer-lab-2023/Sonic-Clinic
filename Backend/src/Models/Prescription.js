259340c5d2c8a9450c47307bb4403eb3bdde4ad9
SarahTawfik <sarahtawfik0447@gmail.com>
2023-10-09 00:59:12 +0300

    array w doc name

--- Backend/src/Models/Prescription.js
+++ Backend/src/Models/Prescription.js
@@ -1,29 +1,35 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const prescriptionSchema = new Schema({
-  medicine: {
-    type: Array,
-    required: true,
+const prescriptionSchema = new Schema(
+  {
+    medicine: {
+      type: Array,
+      required: true,
+    },
+    doctorID: {
+      type: String,
+      required: true,
+    },
+    patientID: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    doctorName: {
+      type: String,
+      required: true,
+    },
   },
-  doctorID: {
-    type: String,
-    required: true
-  },
-  patientID: {
-    type: String,
-    required: true
-  },
-  status: {
-    type: String,
-    required: true
-  },
-  date: {
-    type: Date,
-    required: true
-  }
-
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 const Prescription = mongoose.model('Prescription', prescriptionSchema);
 module.exports = Prescription;
