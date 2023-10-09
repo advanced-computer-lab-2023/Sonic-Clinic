9bcf47f40e8d7326b879540a5591db6e91aac30b
ashrafkaram2002 <ashraf.medhat.karam@gmail.com>
2023-10-09 18:21:34 +0300

    updateprofile

--- Backend/src/Controllers/doctorController.js
+++ Backend/src/Controllers/doctorController.js
@@ -83,13 +83,13 @@ const filterApointmentsByDateOrStatusDoc = async (req, res) => {
 };
 
 const updateDoctorProfile = async (req, res) => {
-  const {email, hourlyRate, affiliation } = req.body;
-  
+  const { email, hourlyRate, affiliation } = req.query; // Extract fields from req.query
+
   const id = req.query._id;
 
   try {
     const doctor = await doctorModel.findOne({ _id: id });
-  
+
     if (!doctor) {
       return res.status(404).json({ message: "Doctor not found." });
     }
@@ -104,9 +104,10 @@ const updateDoctorProfile = async (req, res) => {
 
     res.status(200).json({ message: "Doctor profile updated successfully." });
   } catch (error) {
-    res.status(500).json({ message: "Server Error" ,error});
+    res.status(500).json({ message: "Server Error", error });
   }
 };
+
 
 const viewPatients = async (req, res) => {
   const id = req.body._id;
