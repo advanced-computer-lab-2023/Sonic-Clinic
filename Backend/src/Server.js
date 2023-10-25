e8691d0bd7ca64b1f1710b1b5095f2b3da2e3373
SarahTawfik <sarahtawfik0447@gmail.com>
2023-10-25 09:20:39 +0300

    nfs el methods tany

--- Backend/src/Server.js
+++ Backend/src/Server.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const { MongoClient } = require("mongodb");
 const session = require("express-session");
 const jwt = require("jsonwebtoken");
-const cookieParser=require('cookie-parser');
+const cookieParser = require("cookie-parser");
 
 mongoose.set("strictQuery", false);
 require("dotenv").config();
