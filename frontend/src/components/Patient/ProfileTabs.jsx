53439c47186f3a07d53e895d99276573bd84ce57
youssefbassem1 <youssefbassem12@gmail.com>
2023-11-08 20:52:09 +0200

    edits

--- frontend/src/components/Patient/ProfileTabs.jsx
+++ frontend/src/components/Patient/ProfileTabs.jsx
@@ -7,10 +7,12 @@ import AddFamilyMember from "./AddFamilyMember";
 import FamilyMembersList from "./FamilyMembersList";
 import ViewPersonalInfo from "./viewPersonalInfo";
 import UploadDocuments from "./UploadDocuments";
+import { useSelector } from "react-redux";
 
 function ProfileTabs() {
   const [activeKey, setActiveKey] = useState("first");
   const [refreshFlag, setRefreshFlag] = useState(false);
+  const wallet = useSelector((state) => state.patientLogin.wallet);
 
   const handleSelect = (selectedKey) => {
     setActiveKey(selectedKey);
@@ -103,6 +105,24 @@ function ProfileTabs() {
                   My Wallet
                 </Nav.Link>
               </Nav.Item>
+              <Nav.Item>
+                <Nav.Link
+                  eventKey="fifth"
+                  style={{
+                    background: activeKey === "fifth" ? "#05afb9" : "white",
+                    color: activeKey === "fifth" ? "white" : "black",
+                    border:
+                      activeKey === "fifth"
+                        ? "none"
+                        : "1px solid rgb(189, 189, 189)",
+                    marginBottom: "1rem",
+                    fontSize: "1.2rem",
+                    fontWeight: "600",
+                  }}
+                >
+                  My Packages
+                </Nav.Link>
+              </Nav.Item>
             </Nav>
           </Col>
           <Col lg={9}>
@@ -120,6 +140,35 @@ function ProfileTabs() {
                 <UploadDocuments />
               </Tab.Pane>
               <Tab.Pane eventKey="fourth">
+                <div>
+                  <div
+                    className="d-flex justify-content-center align-items-center"
+                    style={{
+                      fontSize: "2.5rem", // Increase font size for the title
+                      fontWeight: "600",
+                      color: "#212529",
+                      lineHeight: "1.5",
+                    }}
+                  >
+                    My Wallet
+                  </div>
+                  <div
+                    style={{
+                      color: "#099BA0  ",
+                      fontSize: "30px",
+                      fontWeight: "600",
+                      marginBottom: "10px",
+                      marginLeft: "22rem",
+                      marginTop: "3rem",
+                    }}
+                  >
+                    Balance: ${wallet}
+                  </div>
+
+                  {/* You can add more wallet-related content here */}
+                </div>
+              </Tab.Pane>
+              <Tab.Pane eventKey="fifth">
                 <ViewPersonalInfo />
               </Tab.Pane>
             </Tab.Content>
