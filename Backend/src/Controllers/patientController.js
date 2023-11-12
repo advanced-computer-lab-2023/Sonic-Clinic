069e9a5f621886deeaa74eab60314aca688c9f54
ashrafkaram2002 <ashraf.medhat.karam@gmail.com>
2023-11-12 16:02:20 +0200

    aa

--- Backend/src/Controllers/patientController.js
+++ Backend/src/Controllers/patientController.js
@@ -1193,7 +1193,10 @@ let newPackage;
       await mainPatient.save();
     
 
-    return res.status(200).json({ patient });
+      return res.status(200).json({
+        originalPackage: originalPackage,
+        wallet: mainPatient.wallet,
+      });
   } catch (error) {
     console.error("Error:", error);
     return res.status(500).json({ message: "Server Error" });
